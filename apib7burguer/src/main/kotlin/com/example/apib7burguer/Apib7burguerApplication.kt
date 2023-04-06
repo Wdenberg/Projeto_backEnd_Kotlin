4bdb8babfbe82591234f29ff8395e94d0dd75220
wdenb <wdenbergskillshd@hotmail.com>
2023-04-06 15:40:48 -0300

    Upando os Arquivos

---
+++ apib7burguer/src/main/kotlin/com/example/apib7burguer/Apib7burguerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apib7burguer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Apib7burguerApplication
+
+fun main(args: Array<String>) {
+	runApplication<Apib7burguerApplication>(*args)
+}
---
+++ apib7burguer/src/test/kotlin/com/example/apib7burguer/Apib7burguerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apib7burguer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Apib7burguerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
