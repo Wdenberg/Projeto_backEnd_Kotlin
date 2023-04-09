af0819afac431a6094edf02937852ff670d21f3b
DavidjFilho <david_curso@hotmail.com>
2023-04-08 21:28:56 -0300

    correção

--- apib7burguer/src/main/kotlin/com/example/apib7burguer/model/User.kt
+++ apib7burguer/src/main/kotlin/com/example/apib7burguer/model/User.kt
@@ -1,6 +1,6 @@
 package com.example.apib7burguer.model
 
-class class Usuario(val id: Int, val nome: String, var senha: String, val email: String) {
+class Usuario(val id: Int, val nome: String, var senha: String, val email: String) {
     fun cadastro() {
     }
     fun login() {
